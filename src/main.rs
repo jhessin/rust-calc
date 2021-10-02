fde040bf16eda5d99266e53964ec5d4beff10991
Jim Hessin <jhessin@gmail.com>
2021-10-02 12:37:13 -0600

    got a basic menu working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use crate::menu::Operations;
+
+mod calculate_paycheck;
+mod menu;
+mod report_wages;
+
 fn main() {
   println!("Hello, world!");
+
+  // Load modules for each operation and prepare them.
+
+  // Print a menu to the user asking which operation will be done.
+  let operation = menu::show();
+
+  // Operations:
+  match operation {
+    // - Report Wages
+    Operations::ReportWages => report_wages::run(),
+    // - Calculate Paycheck
+    Operations::CalculatePaycheck => calculate_paycheck::run(),
+  }
 }
---
+++ src/report_wages/mod.rs
@@ -0,0 +1,3 @@
+pub fn run() {
+  println!("Running The Report Wages Module");
+}
